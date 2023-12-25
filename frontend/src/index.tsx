f57119f31bf42d53bdfa69400514c05095312c1b
Tadoori Lakshmi Prasanna <lakshmi.tadoori@zemosolabs.com>
2023-12-25 12:35:17 +0530

    feat: Theming

--- frontend/src/index.tsx
+++ frontend/src/index.tsx
@@ -1,6 +1,15 @@
 import ReactDOM from 'react-dom/client';
 import { App } from './App';
+import { ThemeProvider } from '@emotion/react';
+import theme from './theme/theme';
+import { CssBaseline } from '@mui/material';
+import './index.css';
 
 const root = ReactDOM.createRoot(document.getElementById('root') as HTMLElement);
 
-root.render(<App />);
+root.render(
+  <ThemeProvider theme={theme}>
+    <CssBaseline/>
+    <App />
+  </ThemeProvider>
+);
